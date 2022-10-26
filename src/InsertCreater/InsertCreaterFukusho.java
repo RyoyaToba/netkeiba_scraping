df5997d7b52beb0613e353c6a172016a4c75118c
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2022-10-26 11:17:40 +0900

    try-with-resourceへ変更

--- src/InsertCreater/InsertCreaterFukusho.java
+++ src/InsertCreater/InsertCreaterFukusho.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-
 import Create.CreateFukusho;
 import Entity.FukushoEntity;
 import Parts.CreateRaceId;
@@ -15,77 +14,75 @@ import Parts.DBManager;
 
 public class InsertCreaterFukusho {
 
-	public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-		PreparedStatement pstmt = null;
-		String sql = "";
+    String sql = "";
 
-		String year = "2022";
+    String year = "2022";
 
-		int countNumCounter = 0;
-		int dayNumCounter = 0;
+    int countNumCounter = 0;
+    int dayNumCounter = 0;
 
-		CreateRoopCounter createRoopCounter = new CreateRoopCounter();
-		CreateRaceId createRaceId = new CreateRaceId();
-		CreateFukusho createFukusho = new CreateFukusho();
+    CreateRoopCounter createRoopCounter = new CreateRoopCounter();
+    CreateRaceId createRaceId = new CreateRaceId();
+    CreateFukusho createFukusho = new CreateFukusho();
 
-		for (int placeNum = 1; placeNum <= 10; placeNum++) {
-			Map<String, Integer> counterMap = createRoopCounter.createRoopCounter(placeNum);
-			countNumCounter = counterMap.get("countNumCounter");
-			dayNumCounter = counterMap.get("dayNumCounter");
+    for (int placeNum = 1; placeNum <= 10; placeNum++) {
+      Map<String, Integer> counterMap = createRoopCounter.createRoopCounter(placeNum);
+      countNumCounter = counterMap.get("countNumCounter");
+      dayNumCounter = counterMap.get("dayNumCounter");
 
-			/* 回数のループ */
-			for (int countNum = 1; countNum <= countNumCounter; countNum++) {
-				/* 日付のループ */
-				for (int dayNum = 1; dayNum <= dayNumCounter; dayNum++) {
-					/* レースRのループ */
-					for (int raceNum = 1; raceNum <= 12; raceNum++) {
+      /* 回数のループ */
+      for (int countNum = 1; countNum <= countNumCounter; countNum++) {
+        /* 日付のループ */
+        for (int dayNum = 1; dayNum <= dayNumCounter; dayNum++) {
+          /* レースRのループ */
+          for (int raceNum = 1; raceNum <= 12; raceNum++) {
 
-						try {
-							Thread.sleep(1 * 500);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+            try {
+              Thread.sleep(1 * 1000);
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
 
-						/* レースIdを作成 */
-						String raceId = createRaceId.createRaceId(year, placeNum, countNum, dayNum, raceNum);
+            /* レースIdを作成 */
+            String raceId = createRaceId.createRaceId(year, placeNum, countNum, dayNum, raceNum);
 
-						List<FukushoEntity> fukushoList = createFukusho.createFukusho(raceId);
-						FukushoEntity fukusho = null;
+            List<FukushoEntity> fukushoList = createFukusho.createFukusho(raceId);
+            FukushoEntity fukusho = null;
 
-						for (int i = 0; i < fukushoList.size(); i++) {
+            System.err.println(fukushoList);
 
-							fukusho = fukushoList.get(i);
+            // DBへ格納する処理
+            for (int i = 0; i < fukushoList.size(); i++) {
 
-							if (fukusho.getHorseNumber() == null) {
-								continue;
-							}
+              fukusho = fukushoList.get(i);
 
-							Connection con = DBManager.createConnection();
-							try {
-								sql = "INSERT INTO fukusho " + " (race_id, horse_number, payout, popular) "
-										+ " VALUES("
-										+ "'" + fukusho.getRaceId() + "'" + "," + fukusho.getHorseNumber() + ","
-										+ fukusho.getPayOut() + "," + fukusho.getPopular() + ")";
+              if (fukusho.getHorseNumber() == null) {
+                continue;
+              }
 
-								System.out.println("INSERT INTO fukusho "
-										+ " (race_id, horse_number, payout, popular) " + " VALUES(" + "'"
-										+ fukusho.getRaceId() + "'" + "," + fukusho.getHorseNumber() + ","
-										+ fukusho.getPayOut() + "," + fukusho.getPopular() + ")");
+              sql = "INSERT INTO fukusho " + " (race_id, horse_number, payout, popular) "
+                  + " VALUES(" + "'" + fukusho.getRaceId() + "'" + "," + fukusho.getHorseNumber()
+                  + "," + fukusho.getPayOut() + "," + fukusho.getPopular() + ")";
 
-								pstmt = con.prepareStatement(sql);
-								pstmt.executeUpdate();
+              System.out.println(
+                  "INSERT INTO fukusho " + " (race_id, horse_number, payout, popular) " + " VALUES("
+                      + "'" + fukusho.getRaceId() + "'" + "," + fukusho.getHorseNumber() + ","
+                      + fukusho.getPayOut() + "," + fukusho.getPopular() + ")");
 
-							} catch (SQLException e) {
-								e.printStackTrace();
-							} finally {
-								DBManager.closeConnection(con);
+              try (Connection con = DBManager.createConnection();
+                  PreparedStatement pstmt = con.prepareStatement(sql)) {
 
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+                pstmt.executeUpdate();
+
+              } catch (SQLException e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- src/Sample/HorseName.java
+++ src/Sample/HorseName.java
@@ -4,85 +4,84 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
 import Parts.DBManager;
 
 public class HorseName {
 
-	public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-		PreparedStatement pstmt = null;
+    PreparedStatement pstmt = null;
 
-		for (int i = 4573; i <= 5000; i++) {// 4572までとった 全部で26800ページ
-			try {
-				Thread.sleep(1 * 3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+    for (int i = 4573; i <= 4574; i++) {// 4574までとった 全部で26800ページ
+      try {
+        Thread.sleep(1 * 3000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
 
-			String url = "https://db.sp.netkeiba.com/?pid=horse_list&word=&sire=&mare=&bms=&trainer=&owner=&breeder=&under_age=2&over_age=none&prize_min=&prize_max=&retired=1&sort=access&submit=&page="
-					+ i;
-			Document document = Jsoup.connect(url).get();
+      String url =
+          "https://db.sp.netkeiba.com/?pid=horse_list&word=&sire=&mare=&bms=&trainer=&owner=&breeder=&under_age=2&over_age=none&prize_min=&prize_max=&retired=1&sort=access&submit=&page="
+              + i;
+      Document document = Jsoup.connect(url).get();
 
-			// 馬名取得（.DataBox_01 h2）
-			Elements horseNames = document.select(".DataBox_01 h2");
-			Elements genders = document.select(".DataBox_01 p:nth-of-type(1)");
-			Elements fathers = document.select(".DataBox_01 p:nth-of-type(2)");
-			Elements mothers = document.select(".DataBox_01 p:nth-of-type(3)");
-			// System.out.println("horseNames=" + horseNames);// htmlの該当のタグ部分が取得できる
-			for (int j = 0; j < horseNames.size(); j++) {
-				String horseName = horseNames.get(j).text();
+      // 馬名取得（.DataBox_01 h2）
+      Elements horseNames = document.select(".DataBox_01 h2");
+      Elements genders = document.select(".DataBox_01 p:nth-of-type(1)");
+      Elements fathers = document.select(".DataBox_01 p:nth-of-type(2)");
+      Elements mothers = document.select(".DataBox_01 p:nth-of-type(3)");
+      // System.out.println("horseNames=" + horseNames);// htmlの該当のタグ部分が取得できる
+      for (int j = 0; j < horseNames.size(); j++) {
+        String horseName = horseNames.get(j).text();
 
-				if (horseName.contains("'")) {
-					int dotNumber = horseName.indexOf("'");
-					StringBuilder sb = new StringBuilder(horseName);
-					sb.insert(dotNumber, "'");
-					horseName = sb.toString();
-				}
+        if (horseName.contains("'")) {
+          int dotNumber = horseName.indexOf("'");
+          StringBuilder sb = new StringBuilder(horseName);
+          sb.insert(dotNumber, "'");
+          horseName = sb.toString();
+        }
 
-				String gender = genders.get(j).text().substring(0, 1);
-				String birth = genders.get(j).text().substring(2, 7);
-				String father = fathers.get(j).text().substring(2);
+        String gender = genders.get(j).text().substring(0, 1);
+        String birth = genders.get(j).text().substring(2, 7);
+        String father = fathers.get(j).text().substring(2);
 
-				if (father.contains("'")) {
-					int dotNumber2 = father.indexOf("'");
-					StringBuilder sb = new StringBuilder(father);
-					sb.insert(dotNumber2, "'");
-					father = sb.toString();
-				}
+        if (father.contains("'")) {
+          int dotNumber2 = father.indexOf("'");
+          StringBuilder sb = new StringBuilder(father);
+          sb.insert(dotNumber2, "'");
+          father = sb.toString();
+        }
 
-				String mother = mothers.get(j).text().substring(2);
+        String mother = mothers.get(j).text().substring(2);
 
-				if (mother.contains("'")) {
-					int dotNumber3 = mother.indexOf("'");
-					StringBuilder sb = new StringBuilder(mother);
-					sb.insert(dotNumber3, "'");
-					mother = sb.toString();
-				}
+        if (mother.contains("'")) {
+          int dotNumber3 = mother.indexOf("'");
+          StringBuilder sb = new StringBuilder(mother);
+          sb.insert(dotNumber3, "'");
+          mother = sb.toString();
+        }
 
-				Connection con = DBManager.createConnection();
-				try {
-					String sql = "INSERT INTO horse_profile(name,birth_year,gender,father,mother)" + " VALUES" + "("
-							+ "'" + horseName + "'" + "," + "'" + birth + "'" + "," + "'" + gender + "'" + "," + "'"
-							+ father + "'" + "," + "'" + mother + "'" + ")";
+        Connection con = DBManager.createConnection();
+        try {
+          String sql = "INSERT INTO horse_profile(name,birth_year,gender,father,mother)" + " VALUES"
+              + "(" + "'" + horseName + "'" + "," + "'" + birth + "'" + "," + "'" + gender + "'"
+              + "," + "'" + father + "'" + "," + "'" + mother + "'" + ")";
 
-					System.out.println("INSERT INTO horse_profile(name,birth_year,gender,father,mother)" + " VALUES"
-							+ "(" + "'" + horseName + "'" + "," + "'" + birth + "'" + "," + "'" + gender + "'" + ","
-							+ "'" + father + "'" + "," + "'" + mother + "'" + ");");
-					pstmt = con.prepareStatement(sql);
-					pstmt.executeUpdate();
+          System.out.println("INSERT INTO horse_profile(name,birth_year,gender,father,mother)"
+              + " VALUES" + "(" + "'" + horseName + "'" + "," + "'" + birth + "'" + "," + "'"
+              + gender + "'" + "," + "'" + father + "'" + "," + "'" + mother + "'" + ");");
+          pstmt = con.prepareStatement(sql);
+          pstmt.executeUpdate();
 
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					DBManager.closeConnection(con);
-				}
-			}
-			System.err.println(i + "finish!");
-		}
-	}
+        } catch (SQLException e) {
+          e.printStackTrace();
+        } finally {
+          DBManager.closeConnection(con);
+        }
+      }
+      System.err.println(i + "finish!");
+    }
+  }
 }
