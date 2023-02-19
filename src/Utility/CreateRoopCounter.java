91ec589a8e5ac4f118900e9a4e603a770aef1488
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2023-02-19 22:58:29 +0900

    roopcounter

--- src/Utility/CreateRoopCounter.java
+++ src/Utility/CreateRoopCounter.java
@@ -1,6 +1,6 @@
 package Utility;
 
-import Enum.PlaceDetails.Sapporo;
+import Enum.PlaceDetails.*;
 import Enum.PlaceList;
 
 import java.util.HashMap;
@@ -21,15 +21,47 @@ public class CreateRoopCounter {
 				dayNumCounter = Integer.parseInt(Sapporo.getData(targetYear).get("dayNum"));
 				break;
 			case 2:
-
+				countNumCounter = Integer.parseInt(Hakodate.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Hakodate.getData(targetYear).get("dayNum"));
+				break;
+			case 3:
+				countNumCounter = Integer.parseInt(Fukushima.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Fukushima.getData(targetYear).get("dayNum"));
+				break;
+			case 4:
+				countNumCounter = Integer.parseInt(Nigata.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Nigata.getData(targetYear).get("dayNum"));
+				break;
+			case 5:
+				countNumCounter = Integer.parseInt(Tokyo.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Tokyo.getData(targetYear).get("dayNum"));
+				break;
+			case 6:
+				countNumCounter = Integer.parseInt(Nakayama.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Nakayama.getData(targetYear).get("dayNum"));
+				break;
+			case 7:
+				countNumCounter = Integer.parseInt(Chukyo.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Chukyo.getData(targetYear).get("dayNum"));
+				break;
+			case 8:
+				countNumCounter = Integer.parseInt(Kyoto.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Kyoto.getData(targetYear).get("dayNum"));
+				break;
+			case 9:
+				countNumCounter = Integer.parseInt(Hanshin.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Hanshin.getData(targetYear).get("dayNum"));
+				break;
+			case 10:
+				countNumCounter = Integer.parseInt(Kokura.getData(targetYear).get("countNum"));
+				dayNumCounter = Integer.parseInt(Kokura.getData(targetYear).get("dayNum"));
+				break;
 		}
 
-
 		Map<String, Integer> counterMap = new HashMap<>();
-
-
-
-		return null;
+		counterMap.put("countNumCounter", countNumCounter);
+		counterMap.put("dayNumCounter", dayNumCounter);
+		return counterMap;
 	}
 
 
