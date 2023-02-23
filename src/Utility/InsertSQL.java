bc40dd54e4d39b311195aa924366d38fd487c5e7
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2023-02-23 15:41:27 +0900

    InsertSQLCreate

---
+++ src/Utility/InsertSQL.java
@@ -0,0 +1,9 @@
+package Utility;
+
+public class InsertSQL {
+    private static String INSERT_FUKUSYO = "INSERT INTO fuksyo(race_id, horse_number, payout, popular)";
+    private static String INSERT_RACEINFO = "INSERT INTO race_info()";
+    private static String INSERT_RACERESULT = "INSERT INTO race_result()";
+    private static String INSERT_HORSE_DATA = "INSERT INTO horse_data()";
+
+}
